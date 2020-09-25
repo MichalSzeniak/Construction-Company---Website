636add929547d2a75b56f9929a5f946101af655d
Michał Szeniak <michal.szeniak@gmail.com>
2020-09-25 23:43:37 +0200

    feat: part of menu

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,39 @@ import '../scss/main.scss';
 
 
 console.log('HELLO 🚀')
+
+//---
+
+const usersAge = [20, 21, 23, 43];
+
+usersAge.forEach(age => {
+    return console.log(`w przyszłym roku użytkownik bedzie miał ${age + 1} lat`)
+});
+
+//---
+
+// .map tworzy nową tablice 
+
+
+const age = usersAge.map(age => age * 2);
+
+console.log(age);
+
+//---
+
+const users = ["Michał", "Wiktor", "Łukasz"];
+
+const newUser = "Wiesław";
+
+const allUsers = users.concat(newUser);
+console.log(allUsers);
+
+//--- spread \/
+
+const animals = ["rat", "cow"];
+
+const newAnimal = "mouse";
+
+const allAnimals = [...animals, newAnimal, "monkey"];
+
+console.log(allAnimals);
