913d369713da8c9c6309c26925bf1b578303c9bf
Michał Szeniak <michal.szeniak@gmail.com>
2021-01-31 00:59:45 +0100

    feat: interactions

--- src/js/index.js
+++ src/js/index.js
@@ -74,7 +74,6 @@ window.addEventListener('scroll', () => {
       gsap.fromTo(section.children, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: 'easeInOut', scrollTrigger: {
     trigger: section,
     start: 'top 80%',
-    markers: true,
   }})
   })
 
